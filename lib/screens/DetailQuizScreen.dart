6aed434f30d6bd7635d63922a2ed83103b9f12d0
hassenmrakbenjebahi <123837615+hassenmrakbenjebahi@users.noreply.github.com>
2024-02-25 17:19:23 +0100

    add button start quiz

--- lib/screens/DetailQuizScreen.dart
+++ lib/screens/DetailQuizScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class DetailQuizScreen extends StatefulWidget {
   const DetailQuizScreen({Key? key}) : super(key: key);
@@ -32,69 +33,80 @@ class _DetailQuizScreenState extends State<DetailQuizScreen> {
         ),
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(screenWidth * 0.04), // Padding adaptatif
-          child: Card(
-            elevation: 3.0,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(screenWidth * 0.03), // Padding adaptatif
-                  child: Text(
-                    'Détails du quiz',
-                    style: TextStyle(
-                      fontSize: screenWidth * 0.06, // Taille de police adaptative
-                      fontWeight: FontWeight.bold,
+        child: Center( // Utiliser le widget Center pour centrer la Card
+          child: Padding(
+            padding: EdgeInsets.all(screenWidth * 0.04), // Padding adaptatif
+            child: Card(
+              elevation: 3.0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(screenWidth * 0.03), // Padding adaptatif
+                    child: Text(
+                      'Détails du quiz',
+                      style: TextStyle(
+                        fontSize: screenWidth * 0.06, // Taille de police adaptative
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
-               
-                SizedBox(height: 20.0), // Espacement entre les règles du quiz et le reste du contenu
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
-                  child: Row(
-                    children: [
-                      _buildCircleIcon(Icons.check_circle), // Appel à une fonction pour créer l'icône dans un cercle
-                      SizedBox(width: 10), // Espacement entre l'icône et le texte
-                      Text('10 questions'), // Texte de la règle
-                    ],
+                  SizedBox(height: 20.0), // Espacement entre les règles du quiz et le reste du contenu
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
+                    child: Row(
+                      children: [
+                        _buildCircleIcon(Icons.check_circle), // Appel à une fonction pour créer l'icône dans un cercle
+                        SizedBox(width: 10), // Espacement entre l'icône et le texte
+                        Text('10 questions'), // Texte de la règle
+                      ],
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
-                  child: Row(
-                    children: [
-                      _buildCircleIcon(Icons.timer), // Appel à une fonction pour créer l'icône dans un cercle
-                      SizedBox(width: 10), // Espacement entre l'icône et le texte
-                      Text('1 hour'), // Texte de la règle
-                    ],
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
+                    child: Row(
+                      children: [
+                        _buildCircleIcon(Icons.timer), // Appel à une fonction pour créer l'icône dans un cercle
+                        SizedBox(width: 10), // Espacement entre l'icône et le texte
+                        Text('1 hour'), // Texte de la règle
+                      ],
+                    ),
                   ),
-                ),
-                 SizedBox(width: 10),
-                 Padding(
-                  padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
-                  child: Text(
-                    'Please read the conditions carefully so you can understand it:',
-                    style: TextStyle(fontSize: screenWidth * 0.04),
+                  SizedBox(width: 10),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
+                    child: Text(
+                      'Please read the conditions carefully so you can understand it:',
+                      style: TextStyle(fontSize: screenWidth * 0.04),
+                    ),
                   ),
-                ),
-                 SizedBox(width: 10),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
-                  child: Text(
-                    '- Tap on option to select the correct answer',
-                    style: TextStyle(fontSize: screenWidth * 0.04),
+                  SizedBox(width: 10),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
+                    child: Text(
+                      '- Tap on option to select the correct answer',
+                      style: TextStyle(fontSize: screenWidth * 0.04),
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
-                  child: Text(
-                    '- Click submit if you are sure you want to complete all questions',
-                    style: TextStyle(fontSize: screenWidth * 0.04),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
+                    child: Text(
+                      '- Click submit if you are sure you want to complete all questions',
+                      style: TextStyle(fontSize: screenWidth * 0.04),
+                    ),
                   ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                       Get.toNamed('/quiz');
+                    },
+                    child: Text('Start Quiz', style: TextStyle(color: Colors.white)),
+                     style:  ElevatedButton.styleFrom(
+                     backgroundColor: Color.fromARGB(238, 245, 101, 5)
                 ),
-              ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
